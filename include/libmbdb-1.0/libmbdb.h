3e82697f28c6ab3a2b847328c88714e56d4578ea
Joshua Hill <posixninja@crippy-dev.com>
2013-08-07 20:24:14 -0700

    fixed pkgconfig script so it would install, thanks to winocm

---
+++ include/libmbdb-1.0/libmbdb.h
@@ -0,0 +1,17 @@
+/*
+ * libmbdb.h
+ *
+ *  Created on: Jul 17, 2013
+ *      Author: posixninja
+ */
+
+#ifndef LIBMBDB_H_
+#define LIBMBDB_H_
+
+#include <libmbdb-1.0/mbdb.h>
+#include <libmbdb-1.0/backup.h>
+#include <libmbdb-1.0/mbdb_record.h>
+#include <libmbdb-1.0/backup_file.h>
+
+
+#endif /* LIBMBDB_H_ */
